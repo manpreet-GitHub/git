18b7d3ef906f6a4d8a7f5a392448a5f44164d4ea
Manpreet <Manpreet@DEVMAN>
2018-03-05 02:49:49 +0000

    importing changes

--- Active/Active/Ntest.cs
+++ Active/Active/Ntest.cs
@@ -91,11 +91,137 @@ namespace Active
 
         }
 
+        [Test]// This is a second Test to check download pdf button for crossrail
+        public void pdfDownerTest()
+        {
+            String parentWindow = driver.CurrentWindowHandle;
+            // find element with the link words crossrail case study.pdf 
+            driver.FindElement(By.Id("ui-id-3")).Click();
+            //Identifying element by CSS selector. 
+            driver.FindElement(By.CssSelector("a[href*='Downer-Case-Study.pdf']")).Click();
+
+
+            // for each loop for handling parent window
+            foreach (String window in driver.WindowHandles)
+            {
+                driver.SwitchTo().Window(window);
+            }
+
+            // check if it has opened the right page
+            Assert.True(driver.Url.Contains("uploads"));
+            // close the driver 
+            driver.Close();
+            // Back the case study page
+            driver.SwitchTo().Window(parentWindow);
+            // checking if it has navigated properly
+            Assert.True(driver.Title.Contains("Case Studies"));
+        }
+
+        [Test]// this is a third test to navigate through learn more about croosrail case study page
+        public void learnMoreDownerBtn()
+        {
+            String parentWindow = driver.CurrentWindowHandle;
+            driver.FindElement(By.Id("ui-id-3")).Click();
+            // find element with the link words crossrail case study.pdf 
+            driver.FindElement(By.CssSelector("a[href*='downer']")).Click();
+            // check if it has landed to the right page
+            Assert.True(driver.Url.Contains("downer"));
+            // navigate back to the  case study page
+            driver.Navigate().GoToUrl("http://www.sword-activerisk.com/customers/case-studies/");
+
+            //check if the navigation to the case study page was correct.
+            driver.SwitchTo().Window(parentWindow);
+            Assert.True(driver.Title.Contains("Case Studies"));
 
 
 
-    
-        
+        }
+        [Test]// This is a second Test to check download pdf button for crossrail
+        public void netRailPdfTest()
+        {
+            String parentWindow = driver.CurrentWindowHandle;
+            // find element with the link words crossrail case study.pdf 
+            driver.FindElement(By.Id("ui-id-3")).Click();
+            //Identifying element by CSS selector. 
+            driver.FindElement(By.CssSelector("a[href*='Network-Rail-Case-Study.pdf']")).Click();
+
+
+            // for each loop for handling parent window
+            foreach (String window in driver.WindowHandles)
+            {
+                driver.SwitchTo().Window(window);
+            }
+
+            // check if it has opened the right page
+            Assert.True(driver.Url.Contains("uploads"));
+            // close the driver 
+            driver.Close();
+            // Back the case study page
+            driver.SwitchTo().Window(parentWindow);
+            // checking if it has navigated properly
+            Assert.True(driver.Title.Contains("Case Studies"));
+        }
+
+        [Test]// this is a third test to navigate through learn more about croosrail case study page
+        public void netRailLMBtn()
+        {
+            String parentWindow = driver.CurrentWindowHandle;
+            driver.FindElement(By.Id("ui-id-3")).Click();
+            // find element with the link words crossrail case study.pdf 
+            driver.FindElement(By.CssSelector("a[href*='network-rail-transportation']")).Click();
+            // check if it has landed to the right page
+            Assert.True(driver.Url.Contains("network-rail-transportation"));
+            // navigate back to the  case study page
+            driver.Navigate().GoToUrl("http://www.sword-activerisk.com/customers/case-studies/");
+
+            //check if the navigation to the case study page was correct.
+            driver.SwitchTo().Window(parentWindow);
+            Assert.True(driver.Title.Contains("Case Studies"));
+
+        }
+        [Test]// This is a second Test to check download pdf button for crossrail
+        public void slanskaPdfTest()
+        {
+            String parentWindow = driver.CurrentWindowHandle;
+            // find element with the link words crossrail case study.pdf 
+            driver.FindElement(By.Id("ui-id-3")).Click();
+            //Identifying element by CSS selector. 
+            driver.FindElement(By.CssSelector("a[href*='Slanska-Case-Study-2pp-2012-V1.pdf']")).Click();
+
+
+            // for each loop for handling parent window
+            foreach (String window in driver.WindowHandles)
+            {
+                driver.SwitchTo().Window(window);
+            }
+
+            // check if it has opened the right page
+            Assert.True(driver.Url.Contains("uploads"));
+            // close the driver 
+            driver.Close();
+            // Back the case study page
+            driver.SwitchTo().Window(parentWindow);
+            // checking if it has navigated properly
+            Assert.True(driver.Title.Contains("Case Studies"));
+        }
+
+        [Test]// this is a third test to navigate through learn more about croosrail case study page
+        public void slanskaLMBtn()
+        {
+            String parentWindow = driver.CurrentWindowHandle;
+            driver.FindElement(By.Id("ui-id-3")).Click();
+            // find element with the link words crossrail case study.pdf 
+            driver.FindElement(By.CssSelector("a[href*='slanska-2']")).Click();
+            // check if it has landed to the right page
+            Assert.True(driver.Url.Contains("slanska-2"));
+            // navigate back to the  case study page
+            driver.Navigate().GoToUrl("http://www.sword-activerisk.com/customers/case-studies/");
+
+            //check if the navigation to the case study page was correct.
+            driver.SwitchTo().Window(parentWindow);
+            Assert.True(driver.Title.Contains("Case Studies"));
+
+        }
 
     }
-}
+    }
